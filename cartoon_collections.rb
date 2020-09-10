c7530a2b9b65f82498da8371874e0cc5a5911a10
CJ Davis <csdj92@gmail.com>
2020-09-10 20:40:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,8 @@ end
 def find_the_cheese(cheese)
   i = 0
       cheese_types = ["cheddar", "gouda", "camembert"]
-      while i <cheese_types.length
-      return(cheese_types[i])
-    if cheese_types.reject {|i| cheese_types.is_a? (String)}
-      end
-    end
+    while i < cheese_types.length
+    return cheese_types[i] if cheese.include?(cheese_types[i])
+    i += 1
+  end
 end
