e5f53c0e6ef8e9bc057587a4277458d13ec30b95
CJ Davis <csdj92@gmail.com>
2020-09-10 19:42:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(sleepy)
   i=0
-sleepy.each.with_index {|drawf,num| puts "#{num} "
+sleepy.each.with_index {|drawf,num| puts "#{num} #{drawf}"
 end
 
 def summon_captain_planet# code an argument here
